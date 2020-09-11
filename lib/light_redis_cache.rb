5834ba1b1d92f7a305b11e559a628c5fd478230c
eric <eric.leroy-terquem@enercoop.org>
2020-09-11 23:46:07 +0200

    add a basic client class

--- lib/light_redis_cache.rb
+++ lib/light_redis_cache.rb
@@ -1,6 +1,16 @@
 require "light_redis_cache/version"
+require "socket"
 
 module LightRedisCache
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Client
+    attr_accessor :socket
+
+    def initialize hostname:, port:
+      @socket = TCPSocket.new(hostname, port)
+      @hostname = hostname
+      @port = port
+    end
+  end
 end
