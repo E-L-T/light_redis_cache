c1c523642ab15465e23f8189952910c841db42f3
eric <eric.leroy-terquem@enercoop.org>
2020-11-10 00:07:29 +0100

    implement delete_matched method

--- lib/light_redis_cache.rb
+++ lib/light_redis_cache.rb
@@ -24,6 +24,34 @@ module LightRedisCache
     end
 
     # TODO : implement a delete_matched method with https://redis.io/commands/keys
+    def delete_matched matcher
+      #get_keys matcher
+      open_socket
+      @socket.write("*2\r\n$4\r\nKEYS\r\n$#{ matcher.length }\r\n#{ matcher }\r\n")
+      first_result = @socket.gets
+
+      if first_result.include?("*")
+        matched_keys_number = first_result.gsub("*", "").gsub("\r\n", "").to_i
+        keys = []
+        (1..(matched_keys_number*2)).collect do |index|
+          if index.even?
+            keys.push((@socket.gets).gsub("\r\n", ""))
+          else
+            @socket.gets
+          end
+        end
+
+        #delete keys
+        request = ""
+        request_length = 0
+        keys.each do |key|
+          request.insert(-1, "$#{ key.length }\r\n#{ key }\r\n")
+          request_length +=1
+        end
+        @socket.write("*#{ request_length + 1 }\r\n$3\r\nDEL\r\n#{ request }")
+      end
+      close_socket
+    end
 
     private
 
